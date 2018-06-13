d64c7111770f430489114de8a60691837870883e
Sergei Chipiga <chipiga86@gmail.com>
2018-06-13 22:13:08 +0300

    Predict by url

--- ai_task/run.py
+++ ai_task/run.py
@@ -1,13 +1,20 @@
 import os
+import os.path as path
+import pickle
 
-from flask import jsonify
+from flask import jsonify, request
 
 from ai_task.app import app
 from ai_task.db import db, User
 
+model_path = path.join(path.dirname(__file__), "model.reg")
+classifier = pickle.load(open(model_path, "rb"))
+
+
 @app.route("/")
 def greeting():
     return jsonify({"greeting": "Hello man!"})
+
 
 @app.route("/api/v1.0/users")
 def new_user():
@@ -17,13 +24,17 @@ def new_user():
     db.session.commit()
     return jsonify({"username": user.username})
 
+
 @app.route("/api/v1.0/predict")
 def predict():
-    return jsonify({"hello": "world"})
+    comment = request.json.get("comment")
+    return jsonify({"hello": comment})
+
 
 @app.route("/api/v1.0/metrics")
 def metrics():
     return jsonify({"hello": "world"})
+
 
 if __name__ == '__main__':
     if not os.path.exists("db.sqlite"):
