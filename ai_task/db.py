813a72a67f42840db581d92810ae36431a3d508d
Sergei Chipiga <chipiga86@gmail.com>
2018-06-14 07:16:28 +0300

    Utf-8 encoding

--- ai_task/db.py
+++ ai_task/db.py
@@ -1,3 +1,5 @@
+ # -*- coding: utf-8 -*-
+
 from flask_sqlalchemy import SQLAlchemy
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
