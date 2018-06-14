4c01d87493491619d1564edc83fe8055ba290e96
Sergei Chipiga <chipiga86@gmail.com>
2018-06-14 07:14:22 +0300

    Change secret key

--- ai_task/app.py
+++ ai_task/app.py
@@ -6,6 +6,6 @@ __all__ = [
 ]
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "Hello world!"
+app.config["SECRET_KEY"] = "adsh!9a=#&%10=5+1cyfiv@)_m&i0_=5@mk$2*&uyb8o=8&qy04vz#¤"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
